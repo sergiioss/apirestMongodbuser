a2c862cf9875b89eacf717f0dd7e101360307742
sergiioss <sergiiptx@gmail.com>
2022-10-25 11:07:14 +0200

    get

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
+app.get('/hola/:name', (req, res) =>{
+    res.send({message: `Hola ${req.params.name}!`})
+});
 
 app.listen(port, () =>{
     console.log(`API REST corriendo en http://localhost:${port}`);
