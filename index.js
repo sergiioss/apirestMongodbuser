76134d24ffa4521aef1633f8d58a2ab17d60a761
sergiioss <sergiiptx@gmail.com>
2022-10-25 11:43:31 +0200

    Instalando mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -9,10 +10,39 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req, res) =>{
-    res.send({message: `Hola ${req.params.name}!`})
+app.get('/api/product', (req, res) =>{
+    res.send({products:[]})
+    res.status(200).send({message : 'El producto se ha recibido'});
 });
 
-app.listen(port, () =>{
-    console.log(`API REST corriendo en http://localhost:${port}`);
+app.get('/api/product/:productId', (req, res) => {
+
 });
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    res.send(200, {mensaje: 'El producto se ha recibido'});
+
+});
+
+app.put('/api/product/:productId', (req, res) => {
+
+});
+
+app.delete('/api/product/:productId', (req, res) => {
+    
+});
+
+
+mongoose.connect('mongodb://localhost:27017/shop', (error, res) => {
+    if(error){
+       return console.log(`Error al conectara a la base de datos: ${error}`);
+    }else{
+       console.log('Conexion a la base de datos establecida..');
+    }
+
+    app.listen(port, () => {
+        console.log(`API REST corriendo en http://localhost:${port}`);
+    });
+});
+
