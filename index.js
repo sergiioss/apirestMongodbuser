e0bdd444092961e26873055a159d3c3cc9c06967
sergiioss <sergiiptx@gmail.com>
2022-10-27 10:36:13 +0200

    Endpoint de put

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ const Product = require('./models/product');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api/product', (req, res) =>{
-    res.send({products:[]})
-    res.status(200).send({message : 'El producto se ha recibido'});
+app.get('/api/product', (req, res) => {
+    Product.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
+        if (!products) return res.status(404).send({ message: `No existen productos` })
+        res.send(200, { products })
+    })
 });
 
 app.get('/api/product/:productId', (req, res) => {
+    let productId = req.params.productId
 
+    Product.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
+        if (!product) return res.status(404).send({ message: `El producto no existe` })
+        res.status(200).send({ product });
+    })
 });
 
 app.post('/api/product', (req, res) => {
@@ -25,34 +34,48 @@ app.post('/api/product', (req, res) => {
     console.log(req.body)
     let product = new Product()
     product.name = req.body.name,
-    product.picture = req.body.picture,
-    product.price = req.body.price,
-    product.category = req.body.category,
-    product.description = req.body.description
-    
+        product.picture = req.body.picture,
+        product.price = req.body.price,
+        product.category = req.body.category,
+        product.description = req.body.description
+
     product.save((err, productStored) => {
-        if(err) res
-        .status(500)
-        .send({message: 'Error al salvar en la base de datos'})
+        if (err) res
+            .status(500)
+            .send({ message: 'Error al salvar en la base de datos' })
         res.status(200)
-        .send({product: productStored})
+            .send({ product: productStored })
     })
 });
 
 app.put('/api/product/:productId', (req, res) => {
+    let productId = req.params.productId;
+    let update = req.body;
 
+    Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+        if (err) res.status(500).send({message: `Error al actualizar el producto: ${err}`});
+        res.status(200).send({product: productUpdated});
+    });
 });
 
 app.delete('/api/product/:productId', (req, res) => {
-    
+    let productId = req.params.productId;
+
+    Product.findById(productId, (err, product) => {
+        if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` });
+        product.remove(err => {
+            if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` });
+            res.status(200).send({ message: 'El producto ha sido eliminado' });
+        });
+    });
 });
 
 
 mongoose.connect('mongodb://localhost:27017/shop', (error, res) => {
-    if(error){
-       return console.log(`Error al conectara a la base de datos: ${error}`);
-    }else{
-       console.log('Conexion a la base de datos establecida..');
+    if (error) {
+        return console.log(`Error al conectara a la base de datos: ${error}`);
+    } else {
+        console.log('Conexion a la base de datos establecida..');
     }
 
     app.listen(port, () => {
