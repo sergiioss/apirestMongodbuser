411a9df205fb16e168a29b14c496bbcfaa24db27
sergiioss <sergiiptx@gmail.com>
2022-10-27 12:09:09 +0200

    Refactorizando 3

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
 const mongoose = require('mongoose');
 
 const app = require('./app')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000.
+const config = require('./config')
 
-mongoose.connect('mongodb://localhost:27017/shop', (error, res) => {
+mongoose.connect(config.db, (error, res) => {
     if (error) {
         return console.log(`Error al conectara a la base de datos: ${error}`);
     } else {
         console.log('Conexion a la base de datos establecida..');
     }
 
-    app.listen(port, () => {
-        console.log(`API REST corriendo en http://localhost:${port}`);
+    app.listen(config.port, () => {
+        console.log(`API REST corriendo en http://localhost:${config.port}`);
     });
 });
 
