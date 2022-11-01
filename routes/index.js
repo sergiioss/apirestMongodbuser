c1f7b89821634ee0337cf3e25e9c14c9494fc509
sergiioss <sergiiptx@gmail.com>
2022-11-01 17:09:59 +0100

    Middleware

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const api = express.Router();
+const auth = require('../middlewares/auth')
 const ProductCtr = require('../controllers/product');
 
 api.get('/product', ProductCtr.getProducts);
@@ -9,5 +10,8 @@ api.get('/product/:productId', ProductCtr.getProduct);
 api.post('/product', ProductCtr.createProduct);
 api.put('/product/:productId', ProductCtr.updateProduct);
 api.delete('/product/:productId', ProductCtr.deleteProduct);
+api.get('/private', auth.isAuth, function(req,res){
+    res.status(200).send({message: 'Tienes acceso'});
+});
 
 module.exports = api;
