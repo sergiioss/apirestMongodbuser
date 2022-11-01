2817c4f761e433ea94cf56bb7bb3a0a2dbdc9c24
sergiioss <sergiiptx@gmail.com>
2022-11-01 18:14:30 +0100

    private

--- models/user.js
+++ models/user.js
@@ -16,7 +16,6 @@ const UserSchema = new Schema({
 
 UserSchema.pre('save', (next) => {
     let user = this
-    if (!user.isModified('password')) return next();
 
     bcrypt.genSalt(10, (err, salt) => {
         if (err) return next(err)
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@
 
 const express = require('express');
 const api = express.Router();
-const auth = require('../middlewares/auth')
-const ProductCtr = require('../controllers/product');
+const auth = require('../middlewares/auth');
+const userCtrl = require('../controllers/user');
+const productCtr = require('../controllers/product');
 
-api.get('/product', ProductCtr.getProducts);
-api.get('/product/:productId', ProductCtr.getProduct);
-api.post('/product', ProductCtr.createProduct);
-api.put('/product/:productId', ProductCtr.updateProduct);
-api.delete('/product/:productId', ProductCtr.deleteProduct);
+api.get('/product', productCtr.getProducts);
+api.get('/product/:productId', productCtr.getProduct);
+api.post('/product', productCtr.createProduct);
+api.put('/product/:productId', productCtr.updateProduct);
+api.delete('/product/:productId', productCtr.deleteProduct);
+api.post('/signup', userCtrl.signUp);
+api.post('/signin', userCtrl.signIn);
 api.get('/private', auth, (req, res) => {
     res.status(200).send({ message: 'Tienes acceso' });
 });
