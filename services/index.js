dfe328fce3c47ae0c45d97afbe4bebe61e321e9c
sergiioss <sergiiptx@gmail.com>
2022-11-01 16:54:29 +0100

    JWT

---
+++ services/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const jtw = require('jwt-simple');
+const moment = require('moment');
+const config = require('../config');
+
+function createToken (user){
+    const payload = {
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(14, 'days').unix(),
+    }
+
+    return jtw.encode(payload, config.SECRET_TOKEN)
+}
+
+module.exports = createToken;
