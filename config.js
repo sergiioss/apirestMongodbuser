12408a2652dbcb478ff84174abac310fa3113317
sergiioss <sergiiptx@gmail.com>
2022-11-03 17:20:59 +0100

    Intentando deploy 1

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+    /* db: process.env.MONGODB || 'mongodb://localhost:27017/shop', */
+    db: process.env.MONGODB || 'mongodb+srv://sergio:123@cluster0.fq3ekhh.mongodb.net/?retryWrites=true&w=majority',
     SECRET_TOKEN: 'miclavedetokens'
 }
--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const app = require('./app')
 const port = process.env.PORT || 3000.
 const config = require('./config')
 
-mongoose.connect(config.db, (error, res) => {
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URL, (error, res) => {
     if (error) {
         return console.log(`Error al conectara a la base de datos: ${error}`);
     } else {
         console.log('Conexion a la base de datos establecida..');
     }
-
     app.listen(config.port, () => {
         console.log(`API REST corriendo en http://localhost:${config.port}`);
     });
-});
+})
 
